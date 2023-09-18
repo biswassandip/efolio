3d8bff7ff7ac29af8c4860aba764cb90d3317ec3
Sandip Biswas <bizz.sand@gmail.com>
2023-09-18 13:37:12 +0100

    research methods

--- src/components/module_research_methods/rm.jsx
+++ src/components/module_research_methods/rm.jsx
@@ -6,8 +6,8 @@ import { SiProbot } from 'react-icons/si'
 import { AiFillGithub } from 'react-icons/ai'
 import { GiArchiveResearch } from 'react-icons/gi'
 
-// import ARTEFACT1 from '../../artefacts/IA-Digital-Forensics-Prototype.pdf'
-// import ARTEFACT2 from '../../artefacts/IA-Digital-Forensics-Execution.pdf'
+import ARTEFACT1 from '../../artefacts/Discussion-Case-Dark_UX.pdf'
+import ARTEFACT2 from '../../artefacts/Reflective-Ethics_in_computing.pdf'
 
 
 
@@ -28,15 +28,73 @@ const RM = (props) => {
         <div className="container rm__container">
 
           <p>
-            TBC
+            <b>Research methods and professional practice</b> are essential components of the modern world.
+            Conducting scientific investigations with ethical considerations is the foundation of discovering
+            new knowledge, making informed decisions, and progressing professional fields.
+            <br />
+            <br />
+            Ethics in research is essential to ensure the integrity and credibility of <b>scientific work</b>.
+            The module deep dives into the principles that researchers must adhere to ethical principles such as
+            informed consent (Haggerty & Brown, 2016), confidentiality (Gall, Gall, & Borg, 2007), and minimizing harm
+            to participants (Mertens, 2015).
+            <br />
+            <br />
+            Informed consent requires researchers to obtain voluntary and informed agreement from participants
+            before involving them in any study. Confidentiality requires researchers to protect the privacy and anonymity of
+            participants by safeguarding their personal information. Minimizing harm requires researchers to assess potential
+            risks and take steps to mitigate them.
+            <br />
+            <br />
+            The module further enbales in the understanding on a research project that is essential to formulate clear
+            <b>research questions</b>, conduct a comprehensive <b>literature review</b>, and develop a well-structured
+            <b>research proposal</b> (Creswell & Creswell, 2017; Boote & Beile,2005; Locke, Silverman, & Spirduso, 2019).            
+            <br />
+            <br />
+            Further <b>Research questions</b> serve as the guiding framework for the entire research process and should be SMART
+            (specific, measurable, achievable, relevant, and time-bound).
           </p>
 
           <p>
-            TBC
+            Choosing <b>research methods</b> is crucial, depending on questions, data collection, and analysis requirements 
+            (Creswell & Creswell, 2017). Quantitative methods involve collecting and analyzing numerical data to test hypotheses
+            and establish causal relationships (Creswell & Creswell, 2017). Qualitative methods focus on understanding complex 
+            phenomena by collecting and analyzing non-numerical data (Merriam & Tisdell, 2016). Mixed-methods research combines 
+            both approaches to gain a more comprehensive understanding (Creswell & Creswell, 2017).            
+            <br />
+            <br />
+            The module is provides with discussions and tasks that creates an understanding about the <b>Interview, Survery Methods,
+            Questionnaire designs, etc.</b>
+            <br />
+            <br />
+            Interviews involve one-on-one or group discussions with participants to gather information (Kvale & Brinkmann, 2009).
+            <br />
+            <br />
+            Survey methods involve the administration of questionnaires or surveys to a large number of participants (Dillman, Smyth, & Christian,
+            2014). 
+            <br />
+            <br />
+            Surveys are often used to collect quantitative data on a wide range of topics. Researchers design surveys with
+            carefully crafted questions and response options to ensure data validity and reliability.
+            Questionnaire design begins with clear and concise writing of questions (Dillman et al., 2014). Questions should be
+            free from ambiguity, double negatives, and leading language. 
           </p>
 
           <p>
-            TBC
+            Pilot testing is essential to identify and address any issues with the questionnaire (Dillman et al., 2014).
+            Researchers should conduct pretests with a small sample of participants to ensure that questions are easily understood and
+            that the survey flows smoothly.
+            <br />
+            <br />
+            Generalizability relates to the extent to which research findings can be applied to a larger population or context 
+            (Trochim & Donnelly, 2008). To enhance generalizability, researchers should use random sampling techniques and ensure 
+            that their study sample is representative of the larger population of interest. 
+            <br />
+            <br />
+            Scientific research must be ethical and rigorous, with clear questions, comprehensive literature reviews, and well-structured 
+            proposals. The choice of methods depends on the objectives and data requirements. Qualitative methods like case studies, 
+            focus groups, and observations provide rich insights, while interviews and surveys are common data collection methods with 
+            their own strengths and weaknesses. Effective questionnaire design is crucial for reliable survey data. Validity and 
+            generalizability are essential for the credibility and applicability of research findings.            
           </p>
                       
           <div className="rm__LO">
@@ -46,13 +104,62 @@ const RM = (props) => {
               <article className='rm__details'>
                 <IoMdArrowRoundForward className='rm__details-icon'/>
                 <div>
-                  <h4>TBC</h4>
+                  <h4>Understand the ethical principles underpinning scientific investigation</h4>
+                </div>
+              </article>
+
+              <article className='rm__details'>
+                <IoMdArrowRoundForward className='rm__details-icon'/>
+                <div>
+                  <h4>Develop clear and focused research questions</h4>
+                </div>
+              </article>
+
+              <article className='rm__details'>
+                <IoMdArrowRoundForward className='rm__details-icon'/>
+                <div>
+                  <h4>Conduct a thorough literature review to inform research design</h4>
+                </div>
+              </article>
+
+              <article className='rm__details'>
+                <IoMdArrowRoundForward className='rm__details-icon'/>
+                <div>
+                  <h4>Craft a well-structured research proposal to secure funding and approval</h4>
+                </div>
+              </article>
+
+              <article className='rm__details'>
+                <IoMdArrowRoundForward className='rm__details-icon'/>
+                <div>
+                  <h4>Choose appropriate research methods based on research objectives</h4>
+                </div>
+              </article>
+
+              <article className='rm__details'>
+                <IoMdArrowRoundForward className='rm__details-icon'/>
+                <div>
+                  <h4>Gain proficiency in conducting interviews and surveys</h4>
+                </div>
+              </article>
+              
+              <article className='rm__details'>
+                <IoMdArrowRoundForward className='rm__details-icon'/>
+                <div>
+                  <h4>Design questionnaires effectively to gather reliable data</h4>
+                </div>
+              </article>
+
+              <article className='rm__details'>
+                <IoMdArrowRoundForward className='rm__details-icon'/>
+                <div>
+                  <h4>Evaluate and ensure the validity and generalizability of research findings</h4>
                 </div>
               </article>
             </div>
           </div>
 
-          <div className="rm__CO">
+          {/* <div className="rm__CO">
             <h3>Discussions</h3>
             <div className="rm__content">
 
@@ -65,21 +172,29 @@ const RM = (props) => {
             
 
             </div>          
-          </div>
+          </div> */}
 
           <div className="rm__AR">
-          <h3>Assignments</h3>
+          <h3>Assignments & Discussions</h3>
             <div className="rm__content">
 
               <article className='rm__details'>
                 <BsFileEarmarkPdfFill className='rm__details-icon'/>
                 <div>
                   <h4>
-                      TBC
+                      <a href={ARTEFACT1} target='_blank' rel='noopener noreferrer'>Discussion: Dark UX</a>
                   </h4>
                 </div>
               </article>
 
+              <article className='rm__details'>
+                <BsFileEarmarkPdfFill className='rm__details-icon'/>
+                <div>
+                  <h4>
+                      <a href={ARTEFACT2} target='_blank' rel='noopener noreferrer'>Ethics in Computing</a>
+                  </h4>
+                </div>
+              </article>
             </div>          
           </div>        
 
